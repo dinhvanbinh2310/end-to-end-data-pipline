a538d877b3cad475bedc4fc4ddea24d70ead026e
BinhDV <binhdv@kmdtek.com>
2026-09-16 15:26:26 +0700

    test CI succes

--- tests/test_pipeline_sanity.py
+++ tests/test_pipeline_sanity.py
@@ -63,7 +63,7 @@ def test_ci_branch_workflow():
 def test_intentional_failure():
     """Test case cố tình tạo lỗi để xem CI cảnh báo đỏ."""
     expected_status = "SUCCESS"
-    actual_status = "FAILED_INTENTIONALLY"
+    actual_status = "SUCCESS"
     assert actual_status == expected_status, "CỐ TÌNH GÂY LỖI: Trạng thái không đạt chuẩn để test CI!"
 
 
